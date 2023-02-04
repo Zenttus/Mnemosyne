4d0201806a9f34609dae3f6d167904382ddf2cda
dude <sopota@madre.com>
2023-02-04 15:15:09 -0800

    First working prototype

--- main.ts
+++ main.ts
@@ -1,81 +1,80 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Plugin, Notice, TFile, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface MnemosyneSettings {
+	tagsToInclude: string[];
+	tagsToExclude: string[];
+	pathsToInclude: string[];
+	pathToExclude: string[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: MnemosyneSettings = {
+	tagsToInclude: [],
+	tagsToExclude: [],
+	pathsToInclude: [],
+	pathToExclude: []
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Mnemosyne extends Plugin {
+	settings: MnemosyneSettings;
+	mnemosyneSession: MnemosyneSession;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		// Show status bar item when plugin is loaded
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('No Mnemosyne session active.');
 
-		// This adds a simple command that can be triggered anywhere
+		this.mnemosyneSession = new MnemosyneSession();
+
+		// Adds the next command to the command palette
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'get-next-note-mnemosyne',
+			name: 'Get next note',
 			callback: () => {
-				new SampleModal(this.app).open();
+				if (this.mnemosyneSession.sessionActive === false) {
+					window.alert("No session");
+					return;
+				}
+				const nextNote = this.mnemosyneSession.nextNote();
+				if (nextNote) {
+					this.app.workspace.openLinkText(nextNote.basename, nextNote.path, false);
+				}
+				statusBarItemEl.setText("Mnemosyne session active. (" + (this.mnemosyneSession.files.length) + ") notes left.");
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// Adds the skip command to the command palette
+		this.addCommand({
+			id: 'get-skip-note-mnemosyne',
+			name: 'Skip note',
+			callback: () => {
+				if (this.mnemosyneSession.sessionActive === false) {
+					window.alert("No session");
+					return;
+				}
+				const nextNote = this.mnemosyneSession.skipNote();
+				if (nextNote) {
+					this.app.workspace.openLinkText(nextNote.basename, nextNote.path, false);
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Adds the start command to the command palette
+		this.addCommand({
+			id: 'start-mnemosyne',
+			name: 'Start Mnemosyne session',
+			callback: () => {
+				this.mnemosyneSession.reset();
+				this.mnemosyneSession.startSession(this.settings);
+				statusBarItemEl.setText("Mnemosyne session active. (" + (this.mnemosyneSession.files.length) + ") notes left.");
+			}
+			
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Settings
+		this.addSettingTab(new MnemosyneSettingTab(this.app, this));
+		
 	}
 
 	onunload() {
@@ -89,28 +88,98 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+	
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class MnemosyneSession {
+	files: TFile[];
+	sessionActive: boolean;
+	settings: MnemosyneSettings;
+
+	constructor() {
+		this.reset();
+		this.sessionActive = false;
+		this.settings = DEFAULT_SETTINGS;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	startSession(settings: MnemosyneSettings) {
+		this.applyFilters(settings.tagsToInclude, settings.tagsToExclude, settings.pathsToInclude, settings.pathToExclude);
+		if(this.files.length === 0) {
+			window.alert("There are no notes.")
+			return;
+		}
+		this.sessionActive = true;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	applyFilters(tagsToInclude: String[], tagsToExclude: String[], pathsToInclude: String[], pathsToExclude: String[]) {
+
+		// Paths
+		if(pathsToInclude.length > 0) {
+			this.files = this.files.filter(file => pathsToInclude.includes(file.path));
+		}
+		this.files = this.files.filter(file => !pathsToExclude.includes(file.path));
+		
+		// Tags
+		if(tagsToInclude.length > 0) {
+			this.files = this.files.filter(file => {
+				for (const tag of tagsToInclude) {
+					if (file.tags.includes(tag)) {
+						return true;
+					}
+				}
+				return false;
+			});
+		}
+		this.files = this.files.filter(file => {
+			let hasTag = false;
+			for (const tag of tagsToExclude) {
+				if (file.tags.includes(tag)) {
+					hasTag = true;
+					break;
+				}
+			}
+			return !hasTag;
+		});
+
 	}
+	
+	nextNote() {
+		if (this.files.length === 0){
+			this.sessionActive = false;
+			this.statusBarItemEl.setText("Mnemosyne session completed.");
+			window.alert("Mnemonic session completed");
+			return null;
+		}
+		return this.files.shift();
+	}
+	
+	skipNote() {
+		if (this.files.length === 0) {
+			this.sessionActive = false;
+			return null;
+		}else if(this.files.length === 1) {
+			window.alert("Can't skip last note, it's last one.")
+			return null;
+		}
+		this.files = this.files.sort(() => Math.random() - 0.5); //Yes ineficient, yes doesn't matter, yes I'm lazy
+		return this.files[-1];
+	}
+	
+	reset() {
+		// Get files
+		this.files = app.vault.getMarkdownFiles()
+		// Random order
+		this.files = this.files.sort(() => Math.random() - 0.5);
+		this.sessionActive = false;
+	}
+	
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class MnemosyneSettingTab extends PluginSettingTab {
+	plugin: Mnemosyne;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Mnemosyne) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +189,45 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Mnemosyne.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Tags to include')
+			.setDesc('Tags to include in the session. If left empty it will include all Tags.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.tagsToInclude.join(','))
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.tagsToInclude = value.split(',').map(tag => tag.trim());
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Tags to exclude')
+			.setDesc('Tags to exclude from the session.')
+			.addText(text => text
+				.setValue(this.plugin.settings.tagsToExclude.join(','))
+				.onChange(async (value) => {
+					this.plugin.settings.tagsToExclude = value.split(',').map(tag => tag.trim());
+					await this.plugin.saveSettings();
+				}));
+		
+		new Setting(containerEl)
+			.setName('Paths to include')
+			.setDesc('Paths to include in the session. If left empty it will include all Paths.')
+			.addText(text => text
+				.setValue(this.plugin.settings.tagsToInclude.join(','))
+				.onChange(async (value) => {
+					this.plugin.settings.pathsToInclude = value.split(',').map(tag => tag.trim());
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Paths to exclude')
+			.setDesc('Paths to exclude from the session.')
+			.addText(text => text
+				.setValue(this.plugin.settings.pathToExclude.join(','))
+				.onChange(async (value) => {
+					this.plugin.settings.pathToExclude = value.split(',').map(tag => tag.trim());
 					await this.plugin.saveSettings();
 				}));
 	}
 }
+
